085a6cac6e26908dfb2b6c00813993acd4c283a1
arxaqapi <arxaqapi@gmail.com>
2019-03-09 18:56:50 +0100

    dark mode test

--- javascript/script.js
+++ javascript/script.js
@@ -17,3 +17,35 @@ window.addEventListener("load", function() {
     }, 750);
 
 }, false);
+/*  tryed to make a dark mode switcher with ONE button, is not working like expected
+function changeMode() {
+	let stylesheet = document.getElementById('stylesheet').href;
+
+	if (stylesheet === 'css/main.css')
+	{
+		stylesheet.setAttribute('href', 'css/main2.css');
+	} else 
+	{
+		stylesheet.setAttribute('href', 'css/main.css');
+	}
+}
+
+
+function changeMode() {
+	var stylesheetHref = document.getElementById('stylesheet').href;
+	if(stylesheetHref == 'css/dark.css') {
+		stylesheetHref = 'css/main.css';
+	} 
+	else {
+		stylesheetHref = 'css/dark.css';
+	}
+}
+
+function changeModeToDark() {
+	document.getElementById('stylesheet').href = 'css/dark.css';
+	document.getElementById('toDark').style.display = 'none'
+}
+function changeModeToMain() {
+	document.getElementById('stylesheet').href = 'css/main.css';
+	document.getElementById('toMain').style.display = 'none'
+}*/

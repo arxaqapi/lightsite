00f323c13f5c045d3a285fd0a2f5b9c2f7021e3d
arxaqapi <arxaqapi@gmail.com>
2019-03-09 14:21:03 +0100

    font/blink/divs/navbar

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function toggle(elementId) {
-	let ele = document.getElementById(elementId);
-	if(ele.style.display == "block") {
-    		ele.style.display = "none";
+	let element = document.getElementById(elementId);
+	if(element.style.display == "block") {
+    		element.style.display = "none";
   	}
 	else {
-		ele.style.display = "block";
+		element.style.display = "block";
 	}
 } 
+
+window.addEventListener("load", function() {
+	let f = document.getElementById('toBlinkUn');
+	let f2 = document.getElementById('toBlinkAQ');
+    setInterval(function() {
+		f.style.display = (f.style.display == 'none' ? '' : 'none');
+		f2.style.display = (f.style.display == 'none' ? '' : 'none');
+    }, 750);
+
+}, false);
